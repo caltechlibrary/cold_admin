70ce6c04bfa4c2c6c815e9cfad79190175a888c9
R. S. Doiel <rsdoiel@gmail>
2024-07-17 16:35:07 -0700

    Quick Save

---
+++ mod.ts
@@ -0,0 +1,11 @@
+/* COLD related packages */
+export { makePage } from "./render.ts";
+export { appInfo, fmtHelp } from "./version.ts";
+export { OptionsProcessor } from "./options.ts";
+export { handlePeople, People } from "./people.ts";
+export { handleGroups, Group } from "./groups.ts";
+export { handleFunders, Funder } from "./funders.ts";
+export { handleSubjects, Subject } from "./subjects.ts";
+export { handleISSN, ISSN } from "./issn.ts";
+export { handleDOIPrefix, DOIPrefix } from "./doi_prefix.ts";
+export { renderHtdocs } from "./build.ts";
