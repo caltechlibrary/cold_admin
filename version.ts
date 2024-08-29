e1b631ef58a92032464059fdec022d9f8f99b25e
R. S. Doiel <rsdoiel@gmail>
2024-08-29 12:26:32 -0700

    merging main with gh-pages

--- version.ts
+++ version.ts
@@ -12,7 +12,7 @@ export const appInfo: {[k: string]: string} = {
   releaseDate: "2024-08-29",
 
   // ReleaseHash, the Git hash when version.go was generated
-  releaseHash: "a1811fa",
+  releaseHash: "45bb0e3",
 
   // licenseText holds a copy of the application license text.
   licenseText: `
